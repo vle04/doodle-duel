d5115acf910783372c61ee120fc60b55740ec629
vina le <vinale@vinas-mbp.myfiosgateway.com>
2026-07-22 00:22:57 -0400

    display player names in room

--- app/room/[code]/page.tsx
+++ app/room/[code]/page.tsx
@@ -13,6 +13,13 @@ export default async function Room({ params }: RoomPageProps) {
     where: {
       code
     },
+    include: {
+      players: {
+        include: {
+          profile: true,
+        },
+      },
+    },
   });
 
   if (!room) {
@@ -21,7 +28,16 @@ export default async function Room({ params }: RoomPageProps) {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-white text-black">
-      <h1>Room {room.code}</h1>
+      <h1 className="text-2xl font-bold mb-4">Room {room.code}</h1>
+
+      <h2 className="text-xl font-semibold mb-2">Players:</h2>
+      <ul>
+        {room.players.map((player) => (
+          <li key={player.id} className="text-lg">
+            {player.profile.username}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
